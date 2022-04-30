e81b5da30cd50cb9a31ef4843a1f98e1a4a1a021
Thirsty-c <1976801103@qq.com>
2022-04-30 21:40:16 +0800

    Initial commit

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -35,7 +35,10 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**",
+                "/user/sendMsg",//移动端发送短信
+                "/user/login" //移动端登录
         };
 
 //        2.判断本次请求是否需要处理
@@ -48,12 +51,23 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //4.判断登入状态，如果已登录，则直接放行
+        //4-1.判断登入状态，如果已登录，则直接放行
         if (request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为：{}" ,request.getSession().getAttribute("employee"));
 
            Long empId = (Long) request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
+
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4-2.判断登入状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为：{}" ,request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
 
             filterChain.doFilter(request,response);
             return;
